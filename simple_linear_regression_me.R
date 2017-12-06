704b7a62681758f9857f2a345237edb6af93aa53
alibaltschun <alibaltschun@gmail.com>
2017-12-06 14:14:44 +0700

    R visualising

--- simple_linear_regression_me.R
+++ simple_linear_regression_me.R
@@ -18,3 +18,29 @@ test_set = subset(dataset, split == FALSE)
 # Fitting simple Linear regression
 regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor,newdata = test_set)
+
+# Visualising the Training set result
+# install.packages('ggplot2')
+# library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience , y = training_set$Salary),
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience , predict(regressor, newdata = training_set)),
+            color = 'blue') +
+  ggtitle('Salary vs Experience (Training sets)' ) +
+  xlab('Salary') +
+  ylab('Experience')
+
+
+# Visualising the Test set result
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience , y = test_set$Salary),
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience , predict(regressor, newdata = training_set)),
+            color = 'blue') +
+  ggtitle('Salary vs Experience (Test sets)' ) +
+  xlab('Salary') +
+  ylab('Experience')
