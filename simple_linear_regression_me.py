b1d9b10495c3e7ebeccae987827af8004d9e286c
alibaltschun <alibaltschun@gmail.com>
2017-12-06 13:05:50 +0700

    python regressor

--- simple_linear_regression_me.py
+++ simple_linear_regression_me.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, 0].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,3 +21,7 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting simple liniear regression to the trainning set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression(X_train,y_train)
